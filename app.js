2be9c3d7146869251c275d6709bf8bbe2e0aec79
x320630@gruposantander.com <fernando.leites@dbccompany.com.br>
2020-07-12 15:12:13 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { studentRouter } from './routes/studentRouter.js';
+
+const app = express();
+
+require('dotenv').config();
+
+(async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://{$process.env.USERDB}:{$process.env.PWDDB}@bootcamp.t3954.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        );
+    } catch (error) {
+        console.log('Erro ao conectar no MongoDB');
+    }
+})();
+
+
+
+app.use(express.json());
+app.use(studentRouter);
+
+app.listen(3000, () => console.log('rodando'));
